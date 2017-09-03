d0f4e6c642874b136cd95f66734632f63e1a0671
Thiyagarajan s <31199439+thiyagas92@users.noreply.github.com>
2017-09-03 12:28:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
 app.get('/article-one', function (req, res){
   res.sendFile('article-one is requested here');   
 });
 app.get('/article-two',function (req, res){
   res.sendFile('article-two is requested here');   
 });
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
